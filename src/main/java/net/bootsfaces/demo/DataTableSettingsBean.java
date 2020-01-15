4d957c1849c480affea541341f25b212cb6642b7
jens <info@sebsoftware.de>
2020-01-15 10:17:19 +0100

    Revert changes

--- src/main/java/net/bootsfaces/demo/DataTableSettingsBean.java
+++ src/main/java/net/bootsfaces/demo/DataTableSettingsBean.java
@@ -101,8 +101,6 @@ public class DataTableSettingsBean implements Serializable {
 	
 	private boolean scrollToDemo = false;
 	
-	private boolean lengthChange = true; 
-	
 	@ManagedProperty("#{carPool.carPool}")
 	private List<Car> carPool;
 
@@ -541,13 +539,5 @@ public class DataTableSettingsBean implements Serializable {
 
 	public void setPagingType(boolean pagingType) {
 		this.pagingType = pagingType;
-	}
-	
-	public boolean isLengthChange() {
-		return lengthChange;
-	}
-
-	public void setLengthChange(boolean lengthChange) {
-		this.lengthChange = lengthChange;
 	}
 }
